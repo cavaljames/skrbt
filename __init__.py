ebecaa8e60058a9bd84a1e14c7785d2f2b8a2939
sugar <zhangyu@onesight.com>
2023-02-02 18:19:24 +0800

    修改社交聆听代码结构

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+@File    :   __init__.py.py    
+@Contact :   zhangyu@onesight.com
+
+@Modify Time    @Author @Version    @Description
+------------    ------- --------    -----------
+2023/1/29      zhangyu 1.0         None
+"""
